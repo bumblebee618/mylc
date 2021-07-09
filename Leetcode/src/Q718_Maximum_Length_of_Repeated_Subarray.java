2cac155a58cc615573c40c497729ccb155c5afcc
guccio618 <jackiechan618@gmail.com>
2021-07-08 19:28:16 -0700

    Add questions

--- Leetcode/src/Q718_Maximum_Length_of_Repeated_Subarray.java
+++ Leetcode/src/Q718_Maximum_Length_of_Repeated_Subarray.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * 
 Given two integer arrays A and B, return the maximum length of an subarray that appears in both arrays.
@@ -17,7 +19,72 @@ Note:
 
 public class Q718_Maximum_Length_of_Repeated_Subarray 
 {
+	// Solution 1: Binary Search, time is O(nlogn)
 	public int findLength(int[] nums1, int[] nums2) 
+    {
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0)
+        {
+            return 0;
+        }
+        
+        String nums1Str = convertion(nums1);
+        String nums2Str = convertion(nums2);
+        int left = 1, right = Math.min(nums1.length, nums2.length);
+        
+        while (left < right) 
+        {
+            int mid = left + (right - left)/2;
+            
+            if (binarySearch(nums1Str, nums2Str, mid)) 
+            {
+                left = mid + 1;
+            } 
+            else 
+            {
+                right = mid;
+            }
+        }
+        
+        return binarySearch(nums1Str, nums2Str, left) ? left : left - 1;
+    }
+    
+    private String convertion(int[] nums)
+    {
+        StringBuilder builder = new StringBuilder();
+        
+        for (int num : nums)
+        {
+            builder.append((char) num);
+        }
+        
+        return builder.toString();
+    }
+    
+    private boolean binarySearch(String str1, String str2, int guess) 
+    {
+        Set<String> set = new HashSet<>();
+        
+        for (int i = 0; i <= str1.length() - guess; i++)
+        {
+            set.add(str1.substring(i, i+guess));
+        }
+        
+        for (int i = 0; i <= str2.length() - guess; i++)
+        {
+            if (set.contains(str2.substring(i, i+guess)))
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+	
+    
+    
+	
+	// Solution 2: DP, time is O(n^2)
+	public int findLength2(int[] nums1, int[] nums2) 
     {
         if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0)
         {
